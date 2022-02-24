92582d6da58178e8f18fa38e7814ad596ac63d3b
kazuya29184545 <68612252+kazuya29184545@users.noreply.github.com>
2022-02-24 14:15:43 -0800

    Update hw2.js

--- hw2.js
+++ hw2.js
@@ -1,22 +1,36 @@
-const myFunction = function () {
-    //name
-    let person = prompt("Let's play the sum game. \nPlease enter your name");
-
-    if (person != null) {
-
-        //first number
-        let firstNum = parseInt(prompt("Enter the fist number"));
-        
-        if (firstNum != null) {
-            //second number
-            let secondNum = parseInt(prompt("Enter the second number"));
-            if (secondNum != null) {
-                let total = firstNum + secondNum;
-                document.getElementById("demo").innerHTML =
-                "Hi, " + person + "! The sum of " + firstNum + " + " + secondNum + " is " + total;
-            }
-        }
-        
-    }
+// Create the template for objects that are plans
+function Plan(name, housing, tuition, scholarships) {
+    this.name = name;
+    this.housing = housing;
+    this.tuition = tuition;
+    this.scholarships = scholarships;
+    this.totalCost = function() {
+        return this.housing + this.tuition;
+    };
 }
-myFunction();
+
+
+// Create three plan objects
+let tobitate = new Plan('Tobitate', 13000, 15000, true);
+let ibp = new Plan('IBP', 14000, 13000, true);
+let exchange = new Plan('Exchange', 14000, 0, false);
+
+
+//update the html for the page
+let details1 = tobitate.name + ' cost (housing and tuition): ';
+    details1 += tobitate.totalCost();
+    details1 += ", Scholarships: " + tobitate.scholarships;
+let elPlan1 = document.getElementById('plan1');
+elPlan1.textContent = details1;
+
+let details2 = ibp.name + ' cost (housing and tuition): ';
+    details2 += ibp.totalCost();
+    details2 += ", Scholarships: " + ibp.scholarships;
+let elPlan2 = document.getElementById('plan2');
+elPlan2.textContent = details2;
+
+let details3 = exchange.name + ' cost (housing and tuition): ';
+    details3 += exchange.totalCost();
+    details3 += ", Scholarships: " + exchange.scholarships;
+let elPlan3 = document.getElementById('plan3');
+elPlan3.textContent = details3;
